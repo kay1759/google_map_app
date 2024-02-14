f557aad344fbe46e831364e69b21cbd583f38d95
Katsuyoshi Yabe <kay@wizis.com>
2024-02-14 11:33:18 +1100

    library upgrade (typescript 4 -> 5)

--- frontend/src/js/constants/Config.ts
+++ frontend/src/js/constants/Config.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_MAP_API_KEY = "";
 export const GOOGLE_MAP_CENTRE = {
     lat: -37.8114839,
-    lng: 144.9687451
-}
+    lng: 144.9687451,
+};
 export const GOOGLE_MAP_ZOOM = 14;
--- frontend/src/js/store.ts
+++ frontend/src/js/store.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
+import { configureStore, Tuple } from '@reduxjs/toolkit';
 import createSagaMiddleware from "redux-saga";
 import "regenerator-runtime/runtime";
 import { createLogger } from 'redux-logger';
@@ -6,19 +7,16 @@ import { createLogger } from 'redux-logger';
 import rootSaga from "./sagas";
 import locationReducer from "./domains/locations/reducer";
 
-const rootReducer = combineReducers({
-    locationReducer
-});
+const rootReducer = {
+    locationReducer: locationReducer
+};
 
 const sagaMiddleware = createSagaMiddleware();
 const loggerMiddleware = createLogger();
 
-export default createStore(
-    rootReducer,
-    applyMiddleware(
-	sagaMiddleware,
-        loggerMiddleware
-    )
-);
+export default configureStore({
+    middleware: () => new Tuple(loggerMiddleware, sagaMiddleware),
+    reducer: rootReducer
+  });
 
 sagaMiddleware.run(rootSaga);
